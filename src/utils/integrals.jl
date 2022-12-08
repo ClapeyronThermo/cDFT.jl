86ff094414f4bcc61545583b3d5275eeb00734b6
pw0908 <pw1017@ic.ac.uk>
2022-12-07 22:51:54 -0800

    Change integration method to simpson

--- src/utils/integrals.jl
+++ src/utils/integrals.jl
@@ -1,5 +1,10 @@
 function ∫(f,dz)
-    return sum(f)*dz
+    n = length(f)
+    if n % 2 == 0 
+        return (f[1] + f[n] + 4sum(f[2:2:n]) + 2sum(f[3:2:n-1]))*dz/3
+    else
+        return (f[1] + f[n-1] + 4sum(f[2:2:n-1]) + 2sum(f[3:2:n-2]))*dz/3 + f[n]*dz
+    end
 end
 
 function ∫fdz(f::Vector,z::Vector,z_eval::Float64,lim::Float64)
