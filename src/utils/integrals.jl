f5a10f7532246b72e79df4e6e306f6cd1c9ee65c
pw0908 <pw1017@ic.ac.uk>
2022-12-08 16:08:12 -0800

    Trying different integral methods

--- src/utils/integrals.jl
+++ src/utils/integrals.jl
@@ -1,11 +1,11 @@
 function ∫(f,dz)
-    n = length(f)
-    if n % 2 == 0 
-        return (f[1] + f[n] + 4sum(f[2:2:n]) + 2sum(f[3:2:n-1]))*dz/3
-    else
-        return (f[1] + f[n-1] + 4sum(f[2:2:n-1]) + 2sum(f[3:2:n-2]))*dz/3 + f[n]*dz
-    end
+    return sum(f)*dz
 end
+
+# function ∫(f,dz)
+#     n = length(f)
+#     return (3/8*(f[1] + f[n]) + 7/6*(f[2]+f[n-1]) + 23/24*(f[3]+f[n-2]) + sum(f[4:n-3]))*dz
+# end
 
 function ∫fdz(f::Vector,z::Vector,z_eval::Float64,lim::Float64)
     idx = @. z_eval-lim<=z && z<=z_eval+lim
