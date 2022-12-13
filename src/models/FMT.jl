b71923592d8e3d3757ac887b249d66b8202447d0
pw0908 <pw1017@ic.ac.uk>
2022-12-12 20:15:53 -0800

    Fix the HS functional

--- src/models/FMT.jl
+++ src/models/FMT.jl
@@ -10,25 +10,10 @@ Hard-Sphere Functional derived using Fundamental Measure Theory as presented by 
 
 function F_hs(model::SAFTFunctionalModel,T)
     z = model.coords
-    z_full = model.coords_full
-
-    ρ = model.density
-    ρ_full = model.density_full
-
     HSd = d(model.eosmodel,[],T,[1.])[1]
-    m = model.eosmodel.params.segment.values[1]
-    
-    dz = z[2]-z[1]
-    
-    n = ∫fdz.(Ref(ρ_full),Ref(z_full),z,HSd/2)*N_A
-    n₃ = ∫fz²dz.(Ref(ρ_full),Ref(z_full),z,HSd/2)*π*m*N_A
-    nᵥ = ∫fzdz.(Ref(ρ_full),Ref(z_full),z,HSd/2)*N_A
+    dz = (z[2]-z[1])*HSd
 
-    n₀ = m/HSd*n
-    n₁ = m/2*n
-    n₂ = π*m*HSd*n
-    nᵥ₁ = -m/HSd*nᵥ
-    nᵥ₂ = -2π*m*nᵥ
+    (n₀, n₁, n₂, n₃, nᵥ₁, nᵥ₂)  = weights_hs(model,T,z)
     
     Φ = @. -n₀*log(1-n₃)+(n₁*n₂-nᵥ₂*nᵥ₁)/(1-n₃)+(n₂^3/3-n₂*nᵥ₂*nᵥ₂)*(log(1-n₃)/(12*π*n₃^2)+1/(12*π*n₃*(1-n₃)^2))
 
@@ -39,37 +24,50 @@ function δFδρ_hs(model::SAFTFunctionalModel,T)
     z = model.coords
     z_full = model.coords_full
 
+    HSd = d(model.eosmodel,[],T,[1.])[1]
+    dz = (z[2]-z[1])*HSd
+
+    m = model.eosmodel.params.segment.values[1]
+    
+    idx1 = @. (z[1]-1<=z_full && z_full<=z[end]+1)
+
+    (n₀, n₁, n₂, n₃, nᵥ₁, nᵥ₂)  = weights_hs(model,T,z_full[idx1])
+
+    ∂Φ∂n₀ = @. -log(1-n₃)
+    ∂Φ∂n₁ = @. n₂/(1-n₃)
+    ∂Φ∂n₂ = @. n₁/(1-n₃)+3*(n₂^2-nᵥ₂^2)*
+               (n₃+(1-n₃)^2*log(1-n₃))/(36π*n₃^2*(1-n₃)^2)
+    ∂Φ∂n₃ = @. n₀/(1-n₃)+(n₁*n₂-nᵥ₁*nᵥ₂)/(1-n₃)^2-(n₂^3-3*n₂*nᵥ₂^2)*
+               (n₃*(n₃^2-5*n₃+2)+2*(1-n₃)^3*log(1-n₃))/(36π*n₃^3*(1-n₃)^3)
+    ∂Φ∂nᵥ₁ = @. -nᵥ₂/(1-n₃)
+    ∂Φ∂nᵥ₂ = @. -nᵥ₁/(1-n₃)-n₂*nᵥ₂*(n₃+(1-n₃)^2*log(1-n₃))/(6π*n₃^2*(1-n₃)^2)
+
+    δFδρ_hs_1 = ∫fdz.(Ref(1/HSd*∂Φ∂n₀+1/2*∂Φ∂n₁+π*HSd*∂Φ∂n₂),Ref(z_full[idx1]),z,1/2)*HSd
+    δFδρ_hs_2 = ∫fz²dz.(Ref(π*∂Φ∂n₃),Ref(z_full[idx1]),z,1/2)*HSd^3
+    δFδρ_hs_3 = ∫fzdz.(Ref(1/HSd*∂Φ∂nᵥ₁+2π*∂Φ∂nᵥ₂),Ref(z_full[idx1]),z,1/2)*HSd^2
+    return m*(δFδρ_hs_1+δFδρ_hs_2+δFδρ_hs_3)
+end
+
+function weights_hs(model::SAFTFunctionalModel,T,z_eval)
+    z_full = model.coords_full
+
+    HSd = d(model.eosmodel,[],T,[1.])[1]
+
     ρ = model.density
     ρ_full = model.density_full
 
-    HSd = d(model.eosmodel,[],T,[1.])[1]
     m = model.eosmodel.params.segment.values[1]
-    
-    idx1 = @. (z[1]-HSd<=z_full && z_full<=z[end]+HSd)
-    
-    n = ∫fdz.(Ref(ρ_full),Ref(z_full),z_full[idx1],HSd/2)*N_A
-    n₃ = ∫fz²dz.(Ref(ρ_full),Ref(z_full),z_full[idx1],HSd/2)*π*m*N_A
-    nᵥ = ∫fzdz.(Ref(ρ_full),Ref(z_full),z_full[idx1],HSd/2)*N_A
+
+    n = ∫fdz.(Ref(ρ_full),Ref(z_full),z_eval,1/2)*N_A*HSd
+    n₃ = ∫fz²dz.(Ref(ρ_full),Ref(z_full),z_eval,1/2)*π*m*N_A*HSd^3
+    nᵥ = ∫fzdz.(Ref(ρ_full),Ref(z_full),z_eval,1/2)*N_A*HSd^2
 
     n₀ = m/HSd*n
     n₁ = m/2*n
     n₂ = π*m*HSd*n
     nᵥ₁ = -m/HSd*nᵥ
     nᵥ₂ = -2π*m*nᵥ
-
-    ∂Φ∂n₀ = @. -log(1-n₃)
-    ∂Φ∂n₁ = @. n₂/(1-n₃)
-    ∂Φ∂n₂ = @. n₁/(1-n₃)+3*(n₂^2-nᵥ₂^2)*
-               (n₃+(1-n₃)^2*log(1-n₃))/(36π*n₃^2*(1-n₃)^2)
-    ∂Φ∂n₃ = @. n₀/(1-n₃)+(n₁*n₂-nᵥ₁*nᵥ₂)/(1-n₃)^2-(n₂^3-3*n₂*nᵥ₁*nᵥ₂)*
-               (n₃*(n₃^2-5*n₃+2)+2*(1-n₃)^3*log(1-n₃))/(36π*n₃^3*(1-n₃)^3)
-    ∂Φ∂nᵥ₁ = @. -nᵥ₂/(1-n₃)
-    ∂Φ∂nᵥ₂ = @. -nᵥ₁/(1-n₃)-n₂*nᵥ₂*(n₃+(1-n₃)^2*log(1-n₃))/(6π*n₃^2*(1-n₃)^2)
-
-    δFδρ_hs_1 = ∫fdz.(Ref(1/HSd*∂Φ∂n₀+1/2*∂Φ∂n₁+π*HSd*∂Φ∂n₂),Ref(z_full[idx1]),z,HSd/2)
-    δFδρ_hs_2 = ∫fz²dz.(Ref(π*∂Φ∂n₃),Ref(z_full[idx1]),z,HSd/2)
-    δFδρ_hs_3 = ∫fzdz.(Ref(1/HSd*∂Φ∂nᵥ₁+2π*∂Φ∂nᵥ₂),Ref(z_full[idx1]),z,HSd/2)
-    return m*(δFδρ_hs_1+δFδρ_hs_2+δFδρ_hs_3)
+    return n₀, n₁, n₂, n₃, nᵥ₁, nᵥ₂
 end
 
 export F_hs, δFδρ_hs
--- src/utils/integrals.jl
+++ src/utils/integrals.jl
@@ -1,27 +1,36 @@
+# function ∫(f,dz)
+#     return sum(f)*dz
+# end
+
 function ∫(f,dz)
-    return sum(f)*dz
+    return (f[1]+f[end]+2*sum(f[2:end-1]))*dz/2
 end
 
 # function ∫(f,dz)
-#     n = length(f)
-#     return (3/8*(f[1] + f[n]) + 7/6*(f[2]+f[n-1]) + 23/24*(f[3]+f[n-2]) + sum(f[4:n-3]))*dz
+#     return 1/3*dz*(f[1]+f[end]+4*sum(f[2:2:end-1])+2*sum(f[3:2:end-1]))
 # end
 
 function ∫fdz(f::Vector,z::Vector,z_eval::Float64,lim::Float64)
-    idx = @. z_eval-lim<=z && z<=z_eval+lim
     dz = z[2]-z[1]
-    return ∫(f[idx[:]],dz)
+    N = Int(round(1/dz)*lim)
+    id = findfirst(x->x==z_eval,z)
+    idx = id-N:id+N
+    return ∫(f[idx],dz)
 end
 
 function ∫fzdz(f::Vector,z::Vector,z_eval::Float64,lim::Float64)
-    idx = @. z_eval-lim<=z && z<=z_eval+lim
     dz = z[2]-z[1]
-    return ∫(f[idx[:]].*(z[idx[:]].-z_eval),dz)
+    N = Int(round(1/dz)*lim)
+    id = findfirst(x->x==z_eval,z)
+    idx = id-N:id+N
+    return ∫(f[idx].*(z[idx].-z_eval),dz)
 end
 
 
 function ∫fz²dz(f::Vector,z::Vector,z_eval::Float64,lim::Float64)
-    idx = @. z_eval-lim<=z && z<=z_eval+lim
     dz = z[2]-z[1]
-    return ∫(f[idx[:]].*(lim^2 .-(z[idx[:]].-z_eval).^2),dz)
+    N = Int(round(1/dz)*lim)
+    id = findfirst(x->x==z_eval,z)
+    idx = id-N:id+N
+    return ∫(f[idx].*(lim^2 .-(z[idx].-z_eval).^2),dz)
 end
