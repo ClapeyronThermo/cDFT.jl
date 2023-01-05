ffbaa331b5b58d729a0b9a3ef39a275129f63760
pw0908 <pw1017@ic.ac.uk>
2023-01-05 09:56:37 -0800

    Can now obtain interfacial tensions!

--- src/methods/interfacial_tension.jl
+++ src/methods/interfacial_tension.jl
@@ -3,16 +3,16 @@ function interfacial_tension(model::EoSModel,T)
     (Tc,pc,vc) = crit_pure(model)
     (p,vl,vv) = saturation_pressure(model,T)
     # if T<0.75Tc
-        ρ,z = initial_interfacial_density_profile(model,T,[-25σ,25σ],201)
+        ρ,z = initial_interfacial_density_profile(model,T,[-10σ,10σ],101)
     # else
-        # ρ,z = initial_interfacial_density_profile(model,T,[-25σ,25σ],201)
+    #     ρ,z = initial_interfacial_density_profile(model,T,[-20σ,20σ],201)
     # end
 
     ρ = converge_profile!(model,ρ,T,z)
 
     F = F_tot(model,ρ,T,z)
     μ = Clapeyron.VT_chemical_potential(model,vl,T,[1.])[1]
-    return F*k_B*T-(μ*∫(ρ.(z),ρ.mesh_size)+p*∫(one.(z),ρ.mesh_size))
+    return F*k_B*T-μ*∫(ρ.density,ρ.mesh_size)+p*∫(one.(z),ρ.mesh_size)
 end
 
 export interfacial_tension
--- src/methods/profiles.jl
+++ src/methods/profiles.jl
@@ -1,14 +1,25 @@
-function converge_profile!(model,ρ,T,z;method=NLSolvers.Anderson(50,1, nothing,nothing))
+function converge_profile!(model,ρ,T,z;method=NLSolvers.Anderson(0,50,0.02,nothing))
     ρl = ρ.boundary_conditions[2]
     μ_res = Clapeyron.VT_chemical_potential_res(model,1/ρl,T,[1.])/R̄/T
 
-    function obj(model,ρ,T,z,Gx,x,μ_res,ρl,α)
+    # function obj(model,ρ,T,z,Gx,x,μ_res,ρl,α)
+    #     ρ = ClapeyronDFT.update_profile!(ρ,x)
+    #     Gx .= (1-α).*x+α.*ρl.*exp.(μ_res.-δFδρ_res(model,ρ,T,z))
+    #     println(NLSolvers.norm(Gx.-x))
+    #     return Gx
+    # end
+
+    function obj(model,ρ,T,z,x,μ_res,ρl,α)
         ρ = ClapeyronDFT.update_profile!(ρ,x)
-        Gx .= (1-α).*x+α.*ρl.*exp.(μ_res.-δFδρ_res(model,ρ,T,z))
+        Gx = (1-α).*x+α.*ρl.*exp.(μ_res.-δFδρ_res(model,ρ,T,z))
+        return Gx
     end
 
-    fX = (Gx,x) -> obj(model,ρ,T,z,Gx,x,μ_res,ρl,0.02)
+    fX(x) = obj(model,ρ,T,z,x,μ_res,ρl,0.05)
 
-    r = NLSolvers.fixedpoint!(fX, deepcopy(ρ.density), method; maxiter=1000)
-    return update_profile!(ρ,r.x)
+    r = fixed_point(fX, deepcopy(ρ.density);Algorithm = :Anderson, 
+                                            ConvergenceMetric = norm(output,input) = maximum(abs.(output./input .-1)),
+                                            ConvergenceMetricThreshold=1e-6,
+                                            MaxM=50)
+    return update_profile!(ρ,r.FixedPoint_)
 end
