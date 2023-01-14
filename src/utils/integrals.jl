8043921cf55416fb8e531c5c1adc4dd2d936d16d
longemen3000 <longemen3000@gmail.com>
2023-01-14 02:51:14 -0300

    add support for iterators in ∫

--- src/utils/integrals.jl
+++ src/utils/integrals.jl
@@ -1,31 +1,49 @@
-∫(f,dz) = _∫(f,dz)
 
-function _∫(f::AbstractArray,dz)
+"""
+    ∫(f,dz)
+
+Integrates a collection of points `f`, with constant `dz`, using simpson rule
+
+"""
+∫(f,dz) = _∫(f,dz)
+∫(f,dz,lastidx) = _∫(f,dz,lastidx)
+function _∫(f::AbstractArray,dz::Number,lastidx)
     return 1/3*dz*(f[1]+f[end]+4*sum(@view(f[2:2:end-1]))+2*sum(@view(f[3:2:end-1])))
 end
 
+function _∫(f,dz::Number,last = 0)
+    ∑f = zero(typeof(dz))
+    for (i,fi) in pairs(f)
+        if i == 1 || i == last
+            ∑f += fi
+        else
+            ∑f += (4 - 2*(i % 2)) * fi #4 if is even, 2 if is odd
+        end
+    end
+    return ∑f*dz/3
+end
+
 function ∫ρdz(ρ::DensityProfile,z_eval::Float64,span::StepRangeLen)
-    dz = step(span)
+    dz = step(span)*one(eltype(ρ.density))
     z = z_eval.+span
-    I = ρ.(z)
-    return ∫(I,dz)
+    I = (ρ(zi) for zi in z)
+    return ∫(I,dz,length(z))
 end
 
 function ∫ρzdz(ρ::DensityProfile,z_eval::Float64,span::StepRangeLen)
-    dz = step(span)
+    dz = step(span)*one(eltype(ρ.density))
     z = z_eval.+span
-    I = ρ.(z)
-    return ∫(I.*(z.-z_eval),dz)
+    I = (ρ(zi) * (zi - z_eval) for zi in z)
+    return  ∫(I,dz,length(z))
 end
-
 
 function ∫ρz²dz(ρ::DensityProfile,z_eval::Float64,span::StepRangeLen)
     dz = step(span)
     z = z_eval.+span
-    I = ρ.(z)
-    return ∫(I.*(span[end]^2 .-(z.-z_eval).^2),dz)
+    I = (ρ(zi)*(span[end]^2 - (zi - z_eval)^2) for zi in z)
+    return ∫(I,dz,length(z))
 end
-
+#=
 function ∫fdz(model::FunctionalModel,f::Vector,z::Vector{Float64},z_eval::Float64,lim::Float64)
     ϵerr = sqrt(eps(lim))
     idx = @. z_eval-lim-ϵerr<=z && z<=z_eval+lim+ϵerr
@@ -49,4 +67,4 @@ function ∫fz²dz(model::FunctionalModel,f::Vector,z::Vector{Float64},z_eval::F
     dz = model.domain.mesh_size
 
     return ∫(f[idx[:]].*(lim^2 .-(z[idx[:]].-z_eval).^2),dz)
-end
+end=#
