c678c7047725ee47bbd6a197fc4290d5388778b6
longemen3000 <longemen3000@gmail.com>
2023-01-14 02:20:50 -0300

    ones(length(model)) -> onevec(model)

--- src/utils/base.jl
+++ src/utils/base.jl
@@ -3,3 +3,5 @@ Base.length(model::FunctionalModel) = Base.length(model.eosmodel)
 function F_tot(model::EoSModel,ρ,T,z)
     return F_ideal(model.idealmodel,ρ,T,z)+F_res(model,ρ,T,z)
 end
+
+onevec(model) = Clapeyron.FillArrays.Ones(length(model))
