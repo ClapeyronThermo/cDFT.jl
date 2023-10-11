420be996eb26db19509fd06c1ad894722e727c77
seok <benny@cv-3.com>
2023-10-11 14:19:18 -0800

    J2 J3 Generalized

--- src/models/PPCSAFT.jl
+++ src/models/PPCSAFT.jl
@@ -3,7 +3,7 @@ function F_res(model::PPCSAFTModel,ρ,T,z)
     HSd = d(model,nothing,T,onevec(model))
     dz = ρ[1].mesh_size
     
-    _, ρ̄, _    = weights_hs(model,ρ,z,ψ*HSd)
+    _, ρ̄, _ = weights_hs(model,ρ,z,ψ*HSd)
 
     f(x) = f_mp(model,T,@view(x[@comps]))
     Φ_mp = mapslices(f,ρ̄;dims=2)
@@ -22,7 +22,7 @@ function δFδρ_mp(model::PPCSAFTModel,ρ,T,z)
     HSd = d(model,nothing,T,onevec(model))
     lim = ψ*HSd
 
-    _, ρ̄, _  = weights_hs(model,ρ,z,lim)
+    _, ρ̄, _ = weights_hs(model,ρ,z,lim)
 
     f(x) = f_mp(model,T,@view(x[@comps]))
     df(x) = ForwardDiff.gradient(f,x)
@@ -71,7 +71,6 @@ function f_DD(model::PPCSAFTModel,T,ρ̄,η,x)
 
     ρ̄ = sum(ρ̄)
 
-    # TODO: Optimize the code below: Too many same calculations
     A₂ = 0
     A₂_hash = Dict{Set{Int},Any}()
     for i in @comps
@@ -83,7 +82,7 @@ function f_DD(model::PPCSAFTModel,T,ρ̄,η,x)
             else
                 A₂_ij = x_norm[i]*x_norm[j]*
                     1/σ[i,j]^3*μ̄²[i]*μ̄²[j]*                
-                    J_DD_2ij(m[i],m[j],ϵ[i],ϵ[j],η,T)
+                    J2(m[i],m[j],ϵ[i],ϵ[j],η,T;interaction = "DD")
                 A₂_hash[set] = A₂_ij
                 A₂ += A₂_ij
             end
@@ -105,7 +104,8 @@ function f_DD(model::PPCSAFTModel,T,ρ̄,η,x)
                 else
                     A₃_ijk = x_norm[i]*x_norm[j]*x_norm[k]*
                         1/(σ[i,j]*σ[j,k]*σ[i,k])*μ̄²[i]*μ̄²[j]*μ̄²[k]*
-                        J_DD_3ijk(m[i],m[j],m[k],η)
+                        # J_DD_3ijk(m[i],m[j],m[k],η)
+                        J3(m[i],m[j],m[k],η;interaction = "DD")
                     A₃_hash[set] = A₃_ijk
                     A₃ += A₃_ijk
                 end
@@ -121,43 +121,115 @@ function f_DD(model::PPCSAFTModel,T,ρ̄,η,x)
     return A_DD*ρ̄
 end
 
-function J_DD_2ij(mᵢ,mⱼ,ϵᵢ,ϵⱼ,η,T)
-    ϵ_ij = sqrt(ϵᵢ*ϵⱼ)/T # Dimensionless
-    m_ij = minimum([sqrt(mᵢ*mⱼ), 2.0])
-    corr_a = PPCSAFTconsts[:corr_a]
-    corr_b = PPCSAFTconsts[:corr_b]
-    m1 = 1-1/m_ij
-    m2 = m1*(1-2/m_ij)
+function J2(mᵢ,mⱼ,ϵᵢ,ϵⱼ,η,T;interaction = "DD")
+    corr_consts = NamedTuple()
+    ϵ_ij = sqrt(ϵᵢ*ϵⱼ)/T
+    m_ij = sqrt(mᵢ*mⱼ)
+    i_range = 0:4
 
-    J_DD_2ij = 0
+    if interaction == "DD"
+        corr_consts = DD_consts
+        m_ij = minimum([m_ij, 2.0])
+    elseif interaction == "DQ"
+        corr_consts = DQ_consts
+        i_range = 0:3
+    elseif interaction == "QQ"
+        corr_consts = QQ_consts
+    end
 
-    for n in 0:4
+    m1 = 1. - 1/m_ij
+    m2 = m1 * (1. - 2/m_ij)
+    corr_a = corr_consts[:corr_a]
+    corr_b = corr_consts[:corr_b]
+
+    J_2ij = 0.
+
+    for n ∈ i_range
         a0n, a1n, a2n = corr_a[n+1]
         b0n, b1n, b2n = corr_b[n+1]
         a_nij = a0n + a1n*m1 + a2n*m2
         b_nij = b0n + b1n*m1 + b2n*m2
-        J_DD_2ij += (a_nij+b_nij*ϵ_ij)*η^n
+        J_2ij += (a_nij + b_nij*ϵ_ij) * η^n
     end
 
-    return J_DD_2ij
+    return J_2ij
 end
 
-function J_DD_3ijk(mᵢ,mⱼ,mₖ,η)
-    m_ijk = minimum([cbrt(mᵢ*mⱼ*mₖ), 2.0])
-    corr_c = PPCSAFTconsts[:corr_c]
-    m1 = 1-1/m_ijk
-    m2 = m1*(1-2/m_ijk)
+# function J_DD_2ij(mᵢ,mⱼ,ϵᵢ,ϵⱼ,η,T)
+#     ϵ_ij = sqrt(ϵᵢ*ϵⱼ)/T # Dimensionless
+#     m_ij = minimum([sqrt(mᵢ*mⱼ), 2.0])
+#     corr_a = DD_consts[:corr_a]
+#     corr_b = DD_consts[:corr_b]
+#     m1 = 1-1/m_ij
+#     m2 = m1*(1-2/m_ij)
 
-    J_DD_3ijk = 0
-    for n in 0:4
-        c0n, c1n, c2n = corr_c[n+1]
-        c_nijk = c0n + c1n*m1 + c2n*m2
-        J_DD_3ijk += c_nijk*η^n
+#     J_DD_2ij = 0
+
+#     for n in 0:4
+#         a0n, a1n, a2n = corr_a[n+1]
+#         b0n, b1n, b2n = corr_b[n+1]
+#         a_nij = a0n + a1n*m1 + a2n*m2
+#         b_nij = b0n + b1n*m1 + b2n*m2
+#         J_DD_2ij += (a_nij+b_nij*ϵ_ij)*η^n
+#     end
+
+#     return J_DD_2ij
+# end
+
+function J3(mᵢ,mⱼ,mₖ,η;interaction = "DD")
+    m_ijk = cbrt(mᵢ*mⱼ*mₖ)
+    corr_c = ()
+    m1 = 1. - 1/m_ijk
+    m2 = 0.
+    i_range = 0:4
+
+    if interaction == "DD"
+        corr_c = DD_consts[:corr_c]
+        m_ijk = minimum([m_ijk, 2.0])
+        m1 = 1. - 1/m_ijk
+        m2 = m1 * (1. - 2/m_ijk)
+    elseif interaction == "DQ"
+        corr_c = DQ_consts[:corr_c]
+        i_range = 0:3
+    elseif interaction == "QQ"
+        corr_c = QQ_consts[:corr_c]
+        m2 = m1 * (1. - 2/m_ijk)
     end
-    return J_DD_3ijk
+
+    J_3ijk = 0.
+    if interaction == "DQ"
+        for n ∈ i_range
+            c0n, c1n = corr_c[n+1]
+            c_nijk = c0n + c1n*m1
+            J_3ijk += c_nijk*η^n
+        end
+    else
+        for n ∈ i_range
+            c0n, c1n, c2n = corr_c[n+1]
+            c_nijk = c0n + c1n*m1 + c2n*m2
+            J_3ijk += c_nijk*η^n
+        end
+    end
+
+    return J_3ijk
 end
 
-const PPCSAFTconsts = (
+# function J_DD_3ijk(mᵢ,mⱼ,mₖ,η)
+#     m_ijk = minimum([cbrt(mᵢ*mⱼ*mₖ), 2.0])
+#     corr_c = DD_consts[:corr_c]
+#     m1 = 1-1/m_ijk
+#     m2 = m1*(1-2/m_ijk)
+
+#     J_DD_3ijk = 0
+#     for n in 0:4
+#         c0n, c1n, c2n = corr_c[n+1]
+#         c_nijk = c0n + c1n*m1 + c2n*m2
+#         J_DD_3ijk += c_nijk*η^n
+#     end
+#     return J_DD_3ijk
+# end
+
+const DD_consts = (
     corr_a =
     ((0.3043504,0.9534641,-1.161008),
     (-0.1358588,-1.8396383,4.5258607),
@@ -178,4 +250,47 @@ const PPCSAFTconsts = (
     (-0.8087562,-2.3802636,1.6542783),
     (0.6902849,-0.2701261,-3.4396744),
     (0.,0.,0.))
+)
+
+const QQ_consts = (
+    corr_a = 
+    ((1.2378308, 1.2854109,	1.7942954),
+    (2.4355031,	-11.465615,	0.7695103),
+    (1.6330905,	22.086893,	7.2647923),
+    (-1.6118152, 7.4691383,	94.486699),
+    (6.9771185,	-17.197772,	-77.148458)),
+
+    corr_b = 
+    ((0.4542718, -0.813734, 6.8682675),
+    (-4.5016264, 10.06403, -5.1732238),
+    (3.5858868,	-10.876631, -17.240207),
+    (0., 0., 0.),
+    (0., 0., 0.)),
+
+    corr_c =
+    ((-0.5000437, 2.0002094, 3.1358271),
+    (6.5318692, -6.7838658, 7.2475888),
+    (-16.01478, 20.383246, 3.0759478),
+    (14.42597, -10.895984, 0.),
+    (0., 0., 0.))    
+)
+
+const DQ_consts = (
+    corr_a =
+    ((0.697095, -0.6734593, 0.6703408),
+    (-0.6335541, -1.4258991, -4.3384718),
+    (2.945509, 4.1944139, 7.2341684),
+    (-1.4670273, 1.0266216, 0.)),
+
+    corr_b =
+    ((-0.4840383, 0.6765101, -1.1675601),
+    (1.9704055, -3.0138675, 2.1348843),
+    (-2.1185727, 0.4674266, 0.),
+    (0.,	0.,	0.)),
+
+    corr_c =
+    ((7.846431, -20.72202),
+    (33.427, -58.63904),
+    (4.689111, -1.764887),
+    (0., 0.))
 )
