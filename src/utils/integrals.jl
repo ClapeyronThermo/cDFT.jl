791814fccf3f2f7a66783f7b1ec30e015178529b
pw0908 <pjwalker@caltech.edu>
2024-04-11 20:10:35 -0700

    Attempt to accelerate integrals

--- src/utils/integrals.jl
+++ src/utils/integrals.jl
@@ -23,25 +23,54 @@ function _∫(f,dz::Number,last = 0)
     return ∑f*dz/3
 end
 
-function ∫ρdz(ρ::DensityProfile,z_eval::Float64,span::StepRangeLen)
-    dz = step(span)*one(eltype(ρ.density))
-    z = z_eval.+span
-    I = (ρ(zi) for zi in z)
-    return ∫(I,dz,length(z))
+function ∫ρdz(structure::DFTStructure1DCart,ρ::DensityProfile,z_eval::Float64,span::StepRangeLen)
+    I = 0.
+    
+    z1 = z_eval-span[end]
+    z2 = z_eval+span[end]
+
+    idx1 = _binary_search_interval(ρ.coords, z1)
+    idx2 = _binary_search_interval(ρ.coords, z2)
+
+    if idx1 == 0
+        I += ρ.boundary_conditions[1]*(ρ.coords[1]-z1)
+    else
+        I += evalpoly(ρ.coords[idx1+1]-ρ.coords[idx1],append!([0.],ρ.coeffs[idx1]./[1,2,3,4]))
+        I -= evalpoly(z1-ρ.coords[idx1],append!([0.],ρ.coeffs[idx1]./[1,2,3,4]))
+    end
+
+    if idx2 == structure.ngrid
+        I += ρ.boundary_conditions[2]*(z2-ρ.coords[end])
+    else
+        I += evalpoly(z2-ρ.coords[idx2],append!([0.],ρ.coeffs[idx2]./[1,2,3,4]))
+    end
+
+    for i in idx1+1:idx2-1
+        I += evalpoly(ρ.coords[i+1]-ρ.coords[i],append!([0.],ρ.coeffs[i]./[1,2,3,4]))
+    end
+
+    # println(I)
+
+    return I
+
+    # dz = step(span)*one(eltype(ρ.density))
+    # z = z_eval.+span
+    # I = (ρ(zi) for zi in z)
+    # return ∫(I,dz,length(z))
 end
 
-function ∫ρzdz(ρ::DensityProfile,z_eval::Float64,span::StepRangeLen)
+function ∫ρzdz(structure::DFTStructure1DCart,ρ::DensityProfile,z_eval::Float64,span::StepRangeLen)
     dz = step(span)*one(eltype(ρ.density))
     z = z_eval.+span
     I = (ρ(zi) * (zi - z_eval) for zi in z)
     return  ∫(I,dz,length(z))
 end
 
-function ∫ρz²dz(ρ::DensityProfile,z_eval::Float64,span::StepRangeLen)
+function ∫ρz²dz(structure::DFTStructure1DCart,ρ::DensityProfile,z_eval::Float64,span::StepRangeLen)
     dz = step(span)
     z = z_eval.+span
     I = (ρ(zi)*(span[end]^2 - (zi - z_eval)^2) for zi in z)
-    return ∫(I,dz,length(z))
+    return ∫(I,dz,length(z))*π
 end
 #=
 function ∫fdz(model::FunctionalModel,f::Vector,z::Vector{Float64},z_eval::Float64,lim::Float64)
