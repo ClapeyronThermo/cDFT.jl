3bdef0eb13b3d46e369f31a45bb3d4febf6a07c6
pw0908 <pw1017@ic.ac.uk>
2024-04-12 18:25:37 -0700

    Fix free_energy

--- src/utils/base.jl
+++ src/utils/base.jl
@@ -1,7 +1,7 @@
 Base.length(model::FunctionalModel) = Base.length(model.eosmodel)
 
-function F_tot(model::EoSModel,ρ,T,z)
-    return F_ideal(model.idealmodel,ρ,T,z)+F_res(model,ρ,T,z)
+function free_energy(system::DFTSystem)
+    return F_ideal(system)+F_res(system)
 end
 
 onevec(model) = Clapeyron.FillArrays.Ones(length(model))
