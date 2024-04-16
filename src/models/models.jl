f75b59688439bf3affb280ea1c6b5db40a825394
longemen3000 <longemen3000@gmail.com>
2024-04-16 00:01:18 -0400

    models.jl: remove whitespace

--- src/models/models.jl
+++ src/models/models.jl
@@ -5,12 +5,12 @@ function F_res(system::DFTSystem)
 
     n = evaluate_field(system)
 
-    f(x) = f_res(system,model,x)    
-    
+    f(x) = f_res(system,model,x)
+
     ϕ = zeros(ngrid)
-    
+
     Threads.@threads for i in 1:ngrid
-        ϕ[i] = f(n[:,:,i])
+        ϕ[i] = f(@view n[:,:,i])
     end
 
     return ∫(ϕ,dz)
@@ -28,15 +28,15 @@ function δFδρ_res(system::DFTSystem)
 
     n = evaluate_field(system)
 
-    f(x) = f_res(system,model,x)    
+    f(x) = f_res(system,model,x)
     df(x) = ForwardDiff.gradient(f,x)
 
     δf = zeros(nf,nc,ngrid)
-    
+
     # dx = similar(n,nf)
     Threads.@threads for i in 1:ngrid
         δf[:,:,i] = df(n[:,:,i])
-    end 
+    end
 
     δFδρ_res = zeros(nc,ngrid)
 
@@ -46,7 +46,7 @@ function δFδρ_res(system::DFTSystem)
             lim = system.species.size[i]
             bounds = ρ[i].bounds.+(-lim,lim)
             push!(∂f, DensityProfile(@view(δf[j,i,:]),z,bounds,[δf[j,i,1],δf[j,i,end]]))
-        end        
+        end
         δFδρ_res += integrate_field(system,fields[j],∂f)
     end
 
