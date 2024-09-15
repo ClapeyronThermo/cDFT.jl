9eb846a13fe047082c81593e1d40e116babf64b3
pw0908 <pjwalker@caltech.edu>
2024-09-15 16:46:48 -0700

    Minor fix for simple models

--- src/propagator/ideal.jl
+++ src/propagator/ideal.jl
@@ -1,9 +1,9 @@
-function propagate(system::DFTSystem, propagate::IdealPropagator, δf_res, species_id)
+function propagate(system::DFTSystem, propagate::IdealPropagator, δf_res)
     structure = system.structure
     ngrid = structure.ngrid
     nbeads = sum(system.species.nbeads)
 
-    I1 = ones(Float64, ngrid, nbeads)
+    I1 = ones(Float64, ngrid, nbeads, nbeads)
     I2 = ones(Float64, ngrid, nbeads)
     return I1, I2
 end
