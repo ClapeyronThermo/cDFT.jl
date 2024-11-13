86ba62e1378e34917c001cb5b67555ba58354e3d
pw0908 <pjwalker@caltech.edu>
2024-11-12 19:08:14 -0800

    Add first 2D coordinates

--- src/base/structure.jl
+++ src/base/structure.jl
@@ -4,6 +4,7 @@ abstract type DFTStructure1DCart <: DFTStructure1D end
 abstract type DFTStructure1DSphr <: DFTStructure1D end
 
 abstract type DFTStructure2D <: DFTStructure end
+abstract type DFTStructure2DCart <: DFTStructure2D end
 abstract type DFTStructure3D <: DFTStructure end
 
 """
@@ -117,9 +118,49 @@ struct TwoPhase1DCart <: DFTStructure1DCart
     conditions::Tuple{Float64,Float64}
     ρbulk::Vector{Float64}
     ρbulk2::Vector{Float64}
-    bounds::Vector{Float64}
-    ngrid::Int64
+    bounds::Array{Float64}
+    ngrid::Tuple{Int64}
+end
+
+"""
+    TwoPhase1DCart(conditions::Tuple{Float64,Float64}, ρbulk, ρbulk2, bounds::Vector{Float64}, ngrid::Int64)
+
+The generic structure type used when trying to simulate two-phase interfaces in 1D-cartesian coordinates. Contains:
+- `conditions`: The p, T conditions at which the calculations are performed.
+- `ρbulk`: The bulk density of each species in the first phase.
+- `ρbulk2`: The bulk density of each species in the second phase.
+- `bounds`: Specifies the location of the bounds of the system. The interface will be located in the middle.
+- `ngrids`: The number of grid points used to represent the density profile.
+In the case of the surface tension calculation, the pressure specified must be the saturation pressure at T and x. As the density profiles in the liquid and vapour phases are expected to reach their bulk values, the densities at the bounds are _fixed_ at their bulk values. In general, it is recommends to use a width of about `20L` (`L` being the length scale of the model) and about 201 grid points.
+
+The profiles will be initialised as generic sigmoidals of the form:
+`tanh_prof(x,start,stop,shift,coef) = 1/2*(start-stop)*tanh((x-shift)*coef)+1/2*(start+stop)`
+
+Example:
+```julia
+julia> model = PCSAFT(["water"])
+
+julia> T = 298.15
+
+julia> (p, vl, vv) = saturation_pressure(model, T)
+
+julia> ρ1 = [1.0]./vl
+
+julia> ρ2 = [1.0]./vv
+
+julia> L = length_scale(model)
+
+julia> structure = TwoPhase1DCart((p, T), ρ1, ρ2, [-10L, 10L], 201)
+```
+"""
+struct TwoPhase2DLamCart <: DFTStructure2DCart 
+    conditions::Tuple{Float64,Float64}
+    ρbulk::Vector{Float64}
+    ρbulk2::Vector{Float64}
+    bounds::Array{Float64}
+    ngrid::Tuple{Int64,Int64}
 end
 
 export TwoPhase1DCart, Uniform1DCart, ExternalField1DCart
 export Uniform1DSphr
+export TwoPhase2DLamCart
--- src/structure/two_phase.jl
+++ src/structure/two_phase.jl
@@ -7,10 +7,10 @@ function initialize_profiles(model::EoSModel,structure::TwoPhase1DCart, species)
 
     pure = Clapeyron.split_model(model)
 
-    z = range(first(bounds),last(bounds),ngrid) |> collect
+    z = range(first(bounds),last(bounds),ngrid[1]) |> collect
     L = length_scale(model)
 
-    ρ = zeros(ngrid,sum(species.nbeads))
+    ρ = zeros(ngrid...,sum(species.nbeads))
     for i in @comps
         (Tc, pc, vc) = crit_pure(pure[i])
         for j in @chain(i)
@@ -18,6 +18,43 @@ function initialize_profiles(model::EoSModel,structure::TwoPhase1DCart, species)
                           tanh_prof(z,ρ2[i],ρ1[i],(3*bounds[2]/4+bounds[1]/4),(2.4728-2.3625*T/Tc)/L)*(z>(bounds[2]+bounds[1])/2)
 
             ρ[:,j] = ρ_points
+        end
+    end
+    return ρ
+end
+
+function initialize_profiles(model::EoSModel,structure::TwoPhase2DLamCart, species)
+    bounds = structure.bounds
+    ngrid = structure.ngrid
+    (p, T) = structure.conditions
+    ρ1 = structure.ρbulk
+    ρ2 = structure.ρbulk2
+
+    pure = Clapeyron.split_model(model)
+
+    x = range(first(bounds[1,:]),last(bounds[1,:]),ngrid[1]) |> collect
+    y = range(first(bounds[2,:]),last(bounds[2,:]),ngrid[2]) |> collect
+
+    X = zeros(ngrid[1],ngrid[2])
+    Y = zeros(ngrid[1],ngrid[2])
+
+    for i in 1:ngrid[1]
+        X[i,:] .= x[i]
+    end
+
+    for i in 1:ngrid[2]
+        Y[:,i] .= y[i]
+    end
+    
+    L = length_scale(model)
+
+    ρ = zeros(ngrid[1],ngrid[2],sum(species.nbeads))
+    for i in @comps
+        (Tc, pc, vc) = crit_pure(pure[i])
+        for j in @chain(i)
+            ρ_points = @. tanh_prof(X,ρ1[i],ρ2[i],(bounds[1,2]/4+3*bounds[1,1]/4),(2.4728-2.3625*T/Tc)/L)*(X<=(bounds[1,2]+bounds[1,1])/2) +
+                          tanh_prof(X,ρ2[i],ρ1[i],(3*bounds[1,2]/4+bounds[1,1]/4),(2.4728-2.3625*T/Tc)/L)*(X>(bounds[1,2]+bounds[1,1])/2)
+            ρ[:,:,j] = ρ_points
         end
     end
     return ρ
